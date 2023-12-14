af30825319bd52b414b5a2bada9f65100fa5011f
dev-hippo-an <dkstpgud@gmail.com>
2023-12-14 19:27:43 +0900

    chore: ping and pong 코드 흐름 분석;

--- tcp_session/ping.go
+++ tcp_session/ping.go
@@ -2,6 +2,7 @@ package tcp_session
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"time"
 )
@@ -11,41 +12,44 @@ const defaultPingInterval = 30 * time.Second
 func Pinger(ctx context.Context, w io.Writer, reset <-chan time.Duration) {
 	var interval time.Duration
 	select {
-	case <- ctx.Done():
+	case <-ctx.Done():
 		return
-	case interval = <-reset:  // 1 reset 채널에서 초기 간격을 받아 옴
-	default:
+	case interval = <-reset: // 1 reset 채널에서 초기 간격을 받아 옴 - reset 의 초기 cap 이 1 이기 때문에 블로킹 없이 값을 받아옮
+		fmt.Println("this is initial interval from channel : ", interval)
+	default: // 초기 값이 제공되지 않는 경우 default 로 흐름 진행
+		fmt.Println("this is default select statement.. let's get it")
 	}
 
 	if interval <= 0 {
 		interval = defaultPingInterval
 	}
 
-	timer := time.NewTimer(interval)  // 2
+	timer := time.NewTimer(interval) // 2
 
 	defer func() {
 		if !timer.Stop() {
-			<-timer.C
+			time := <-timer.C
+			fmt.Println("this is time : ", time)
 		}
 	}()
 
-	for {
+	for { // 3가지 케이스 중 하나가 일어날 때까지 블로킹
 		select {
-		case <-ctx.Done():  // 3
+		case <-ctx.Done(): // 3 컨텍스트가 취소된 경우
 			return
-		case newInterval := <-reset:  // 4
+		case newInterval := <-reset: // 4 리셋을 위한 시그널을 받은 경우 인터벌 리셋
 			if !timer.Stop() {
 				<-timer.C
 			}
 			if newInterval > 0 {
 				interval = newInterval
 			}
-		case <-timer.C:  // 5
+		case <-timer.C: // 5 타이머 만료
 			if _, err := w.Write([]byte("ping")); err != nil {
 				return
 			}
 		}
-
-		_ = timer.Reset(interval)  // 6
+		// 이후 다시 루프를 시작하기 전에 interval 로 timer reset 함
+		_ = timer.Reset(interval) // 6
 	}
 }
--- tcp_session/ping_test.go
+++ tcp_session/ping_test.go
@@ -11,6 +11,7 @@ import (
 func TestPingAdvanceDeadline(t *testing.T) {
 	done := make(chan struct{})
 
+	// 리스너 설정
 	listener, err := net.Listen("tcp", "127.0.0.1:")
 	if err != nil {
 		t.Fatal(err)
@@ -35,7 +36,7 @@ func TestPingAdvanceDeadline(t *testing.T) {
 			conn.Close()
 		}()
 
-		resetTimer := make(chan time.Duration, 1)
+		resetTimer := make(chan time.Duration, 1) // cap 1개 블로킹 없이 코드 진행
 		resetTimer <- time.Second
 		go Pinger(ctx, conn, resetTimer)
 
@@ -48,14 +49,17 @@ func TestPingAdvanceDeadline(t *testing.T) {
 		buf := make([]byte, 1024)
 
 		for {
+			// connection 에 write 되는 타이밍은 처음 4번의 read 가 종료되고 난 후 - 90번째 라인 코드
+			// 4초가 된 후에 write 가 발생하고 read 가 되면서 아래에서 connection 의 데드라인을 다시 5초로 설정
 			n, err := conn.Read(buf)
 			if err != nil {
 				return
 			}
-			t.Logf("[%s] %s", time.Since(begin).Truncate(time.Second), buf[:n])
+			t.Logf("In listener accept go routine -> [%s] %s", time.Since(begin).Truncate(time.Second), buf[:n])
 
-			resetTimer <- 0
+			//resetTimer <- 0
 
+			// 5초의 타임아웃이 설정되고나서 done 채널이 close 됨
 			err = conn.SetDeadline(time.Now().Add(5 * time.Second))
 			if err != nil {
 				t.Error(err)
@@ -64,6 +68,7 @@ func TestPingAdvanceDeadline(t *testing.T) {
 		}
 	}()
 
+	// 리스너에 연결 설정
 	conn, err := net.Dial("tcp", listener.Addr().String())
 
 	if err != nil {
@@ -73,18 +78,23 @@ func TestPingAdvanceDeadline(t *testing.T) {
 	defer conn.Close()
 
 	buf := make([]byte, 1024)
-	for i := 0; i < 4; i++ {
-		n, err := conn.Read(buf)
+
+	for w := 0; w < 3; w++ {
+		for i := 0; i < 4; i++ {
+			// ping.go 의 타이머 만료로 ping 이 connection 에 write 된다.\
+			// 해당 값을 읽어 오는 것
+			n, err := conn.Read(buf)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			t.Logf("In first ping test - [%s] %s", time.Since(begin).Truncate(time.Second), buf[:n])
+		}
+
+		_, err = conn.Write([]byte("Pong!"))
 		if err != nil {
 			t.Fatal(err)
 		}
-
-		t.Logf("[%s] %s", time.Since(begin).Truncate(time.Second), buf[:n])
-	}
-
-	_, err = conn.Write([]byte("Pong!"))
-	if err != nil {
-		t.Fatal(err)
 	}
 
 	for i := 0; i < 4; i++ {
@@ -95,15 +105,15 @@ func TestPingAdvanceDeadline(t *testing.T) {
 			}
 			break
 		}
-		t.Logf("[%s] %s", time.Since(begin).Truncate(time.Second), buf[:n])
+		t.Logf("In second ping test -> [%s] %s", time.Since(begin).Truncate(time.Second), buf[:n])
 
 	}
 
-	<-done
+	<-done // 읽어올 값이 없어 대기하다가 done 채널이 close 가 된 후 (위의 고루틴의 자원 정리 함수에서 진행) 흐름 진행
 	end := time.Since(begin).Truncate(time.Second)
 
 	t.Logf("[%s] done", end)
-	if end != 9*time.Second {
+	if end != 17*time.Second {
 		t.Fatalf("expected EOF at 9 seconds; actual %s", end)
 	}
 }
